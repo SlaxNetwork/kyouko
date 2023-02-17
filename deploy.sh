dfe3644e368356f585d633c22a1cab6e1eb6627f
auth <65650599+Authorises@users.noreply.github.com>
2023-02-17 17:58:24 +0000

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+FILE=/etc/kyouko/docker-compose.yml
+docker pull ghcr.io/slaxnetwork/kyouko:main
+if test -f "$FILE"; then
+    docker compose -f $FILE stop
+fi
+sudo curl "https://raw.githubusercontent.com/SlaxNetwork/kyouko/main/docker-compose.yml" --create-dirs -o $FILE
+docker compose -f $FILE up -d
