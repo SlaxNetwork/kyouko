1be121d7e567983c6d25a291b406e3c45bdadac2
Tech <debitcardzpaypal@gmail.com>
2023-02-17 15:36:45 -0500

    misc: update IconsConfig type

--- src/configs/icons-config.ts
+++ src/configs/icons-config.ts
@@ -3,6 +3,7 @@ import { loadConfig } from "./config-utils";
 
 type IconsConfig = {
     ranks: { [key: string]: string };
+    emojis: { [key: string]: string };
 };
 
 export default loadConfig("icons.json") as IconsConfig;
