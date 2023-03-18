fd4b8bd45a58c3b2ad0f211dda75355875c7d37c
Matthew <mwmatthew10@gmail.com>
2023-03-18 00:09:23 +0000

    Update response-wrapper.interceptor.ts (#11)

--- src/interceptors/response-wrapper.interceptor.ts
+++ src/interceptors/response-wrapper.interceptor.ts
@@ -1,40 +1,22 @@
 import { Action, Interceptor, InterceptorInterface } from "routing-controllers";
 import { Service } from "typedi";
 
-type KyoukoResponseBody = {
-    error: boolean;
-    data?: any;
-
-    message?: string;
-    status?: number;
-};
-
 // Interceptor breaks @OnUndefined and @OnNull :(
 @Interceptor()
 @Service()
 class WrapResponseInterceptor implements InterceptorInterface {
     intercept(action: Action, result: any) {
-        /**
-         * Check whether a response being sent was a success or not.
-         */
-        const isSuccess = (code: number) => code >= 200 && code < 300;
-
-        let body: KyoukoResponseBody;
-
-        if (isSuccess(action.response.statusCode)) {
-            body = {
+        if (action.response.statusCode >= 200 && action.response.statusCode < 300 ) {
+            return {
                 error: false,
                 data: result
             };
-        } else {
-            body = {
-                error: true,
-                message: result.message || undefined,
-                status: result.status || 500
-            };
         }
-
-        return body;
+        return {
+            error: true,
+            message: result.message || undefined,
+            status: result.status || 500
+        };
     }
 }
 
