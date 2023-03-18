1220b030a3e6d30920785aeff0092ccdb18d4358
Matthew <mwmatthew10@gmail.com>
2023-03-18 00:08:51 +0000

    Update profile-controller.ts (#14)

--- src/controllers/profile-controller.ts
+++ src/controllers/profile-controller.ts
@@ -1,7 +1,6 @@
-import { Authorized, Get, JsonController, QueryParams } from "routing-controllers";
+import { Authorized, Get, JsonController, QueryParams, NotFoundErrror } from "routing-controllers";
 import { httpError } from "../utils/error-utils";
 import { Profile } from "@prisma/client";
-import { notFoundError } from "./errors/generc.error";
 import { IsOptional, IsString, IsUUID } from "class-validator";
 import { Service } from "typedi";
 import { ProfileService } from "../services/postgres/profile-service";
@@ -27,27 +26,16 @@ class ProfileController {
     async getProfile(@QueryParams() query: GetProfileQuery) {
         const { uuid, username } = query;
 
-        let profile: Profile | null = null;
-
-        if (uuid) {
-            profile = await this.profileService.findByUUID(uuid);
-        }
+        let profile: Profile | null = uuid ? await this.profileService.findByUUID(uuid) : null;
 
         if (username && !profile) {
             const cachedUuid = await this.redisService.getUUIDFromName(username);
-            if (!cachedUuid) {
-                throw httpError({
-                    message: `no uuid cached for ${username}`,
-                    httpCode: 404
-                });
-            }
+            if (!cachedUuid) throw new NotFoundError(`No UUID cached for ${username}`);
 
             profile = await this.profileService.findByUUID(cachedUuid);
         }
 
-        if (!profile) {
-            throw notFoundError;
-        }
+        if (!profile) throw new NotFoundErrror("resource not found");
 
         return profile;
     }
