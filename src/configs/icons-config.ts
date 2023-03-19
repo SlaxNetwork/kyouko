d2fd1a0eedd3547de15da9508b0bf99d77e1ad84
Tech <debitcardzpaypal@gmail.com>
2023-03-19 12:31:41 -0400

    refactor: remove icons

--- src/configs/icons-config.ts
+++
@@ -1,8 +0,0 @@
-import { loadConfig } from "./config-utils";
-
-type IconsConfig = {
-    ranks: { [key: string]: string };
-    emojis: { [key: string]: string };
-};
-
-export default loadConfig("icons.json") as IconsConfig;
--- src/services/memory/icon-service.ts
+++ src/services/memory/icon-service.ts
@@ -1,6 +1,5 @@
 import { Service } from "typedi";
 import { flattenObject } from "../../utils/object-utils";
-import iconsConfig from "../../configs/icons-config";
 
 type Icon = {
     name: string;
@@ -12,7 +11,7 @@ class IconService {
     getAll(): Icon[] {
         // Flatten object will turn this { a: { b: 1 } } into
         // { a_b: 1 }, we use this to properly map icon names to their character.
-        const flatObj = flattenObject(iconsConfig);
+        const flatObj = flattenObject({});
 
         // list of icon pairs
         return Object.keys(flatObj).map((k) => ({ name: k, char: flatObj[k] } as Icon));
