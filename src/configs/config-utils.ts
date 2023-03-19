6b8e881affa19c2481467c770bb8eaae646b640d
Tech <49742865+DebitCardz@users.noreply.github.com>
2023-03-19 12:36:33 -0400

    refactor: remove icons (#20)

--- src/configs/config-utils.ts
+++
@@ -1,8 +0,0 @@
-import fs from "fs";
-import path from "path";
-
-function loadConfig(name: string): unknown {
-    return JSON.parse(fs.readFileSync(path.join(__dirname, "../../configs", name), "utf8"));
-}
-
-export { loadConfig };
