24fcce551a2fd81b735f449e422405d02f045465
Tech <debitcardzpaypal@gmail.com>
2023-03-20 21:17:34 -0400

    refactor: remove icon controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,10 +1,7 @@
-// Controllers marked as V1.
-
 import { ProfileController } from "./profile-controller";
 import { CookieClickerController } from "./game/cookie-clicker-controller";
 import { SessionController } from "./session-controller";
 import { ServerController } from "./server-controller";
-import { IconController } from "./icon-controller";
 import { RankController } from "./rank-controller";
 import { HealthController } from "./health-controller";
 
@@ -13,7 +10,6 @@ export default [
     CookieClickerController,
     SessionController,
     ServerController,
-    IconController,
     RankController,
     HealthController
 ];
